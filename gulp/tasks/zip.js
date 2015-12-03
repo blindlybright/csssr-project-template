40e954d45f33df7164336f0bc192b124ba03c0d4
blindlybright <blindlybright@gmail.com>
2015-12-04 02:48:54 +0300

    feat(zip): add generation of separate archives for app & dist folders

--- gulp/tasks/zip.js
+++ gulp/tasks/zip.js
@@ -1,26 +1,47 @@
-import gulp from 'gulp';
-import gutil from 'gulp-util';
-import zip from 'gulp-zip';
-import paths from '../paths';
+import gulp   from 'gulp';
+import gutil  from 'gulp-util';
+import gulpif from 'gulp-if';
+import zip    from 'gulp-zip';
+import paths  from '../paths';
+import pkg    from '../../package.json';
 
-const correctNumber = number => number < 10 ? '0' + number : number;
+let correctNumber = (number) => number < 10 ? '0' + number : number;
 
-const getDateTime = () => {
-	const now = new Date();
-	const year = now.getFullYear();
-	const month = correctNumber(now.getMonth() + 1);
-	const day = correctNumber(now.getDate());
-	const hours = correctNumber(now.getHours());
-	const minutes = correctNumber(now.getMinutes());
+let getDateTime = () => {
+	let now = new Date();
+	let year = now.getFullYear();
+	let month = correctNumber(now.getMonth() + 1);
+	let day = correctNumber(now.getDate());
+	let hours = correctNumber(now.getHours());
+	let minutes = correctNumber(now.getMinutes());
 
-	return `${year}-${month}-${day}-${hours}${minutes}`;
+	return year + '-' + month + '-' + day + '-' + hours + '' + minutes;
 };
 
-gulp.task('zip', () => {
-	const datetime = gutil.env.zipDateTime ? '-' + getDateTime : '';
-	const zipName = 'dist' + datetime + '.zip';
+gulp.task('zip:dist', () => {
+	let zipDistName = pkg.name + '-dist-' + getDateTime() + '.zip';
 
-	gulp.src('dist/**/*')
-		.pipe(zip(zipName))
-		.pipe(gulp.dest(paths.dist));
+	return gulp.src([
+			'dist/**/*',
+			'!**/*.zip'
+		])
+		.pipe(zip(zipDistName))
+		.pipe(gulp.dest(paths.dist))
 });
+
+gulp.task('zip:app', () => {
+	let zipAppName = pkg.name + '-app-' + getDateTime() + '.zip';
+
+	return gulp.src([
+		'**/*',
+		'./.*',
+		'!**/*.{zip,sublime-*}',
+		'!{node_modules,node_modules/**/*}',
+		'!{dist,dist/**/*}',
+		'!{.git,.git/**/*}'
+	])
+		.pipe(zip(zipAppName))
+		.pipe(gulp.dest(paths.dist))
+});
+
+gulp.task('zip', ['zip:app', 'zip:dist']);
